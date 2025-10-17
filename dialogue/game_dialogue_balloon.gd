25c5313416544dafbbb22fc97d851c24fbf8e855
YCD-Jerry <heliex626@gmail.com>
2025-10-17 23:58:08 +0800

    10172358

---
+++ dialogue/game_dialogue_balloon.gd
@@ -0,0 +1,3 @@
+extends BaseGameDialogueBalloon
+
+@onready var emotes_panel: Panel = $MarginContainer/VBoxContainer/EmotesPanel
--- scenes/characters/guide/guide.gd
+++ scenes/characters/guide/guide.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-var balloon_scene = preload("res://dialouge/game_dialogue_balloon.tscn")
+var balloon_scene = preload("res://dialogue/game_dialogue_balloon.tscn")
 
 @onready var interactable_component: InteractableComponent = $InteractableComponent
 @onready var interactable_label_component: Control = $InteractableLabelComponent
@@ -11,6 +11,8 @@ func _ready() -> void:
 	interactable_component.interactable_activated.connect(on_interactable_activated)
 	interactable_component.interactable_deactivated.connect(on_interactable_deactivated)
 	interactable_label_component.hide()
+
+	GameDialogueManager.give_crop_seeds.connect(on_give_crop_seeds)
 
 func on_interactable_activated() -> void:
 	interactable_label_component.show()
@@ -26,4 +28,10 @@ func _unhandled_input(event: InputEvent) -> void:
 		if event.is_action_pressed("show_dialogue"):
 			var balloon: BaseGameDialogueBalloon = balloon_scene.instantiate()
 			get_tree().current_scene.add_child(balloon)
-			balloon.start(load("res://guide.dialogue"),"start")
+			balloon.start(load("res://dialogue/conversations/guide.dialogue"), "start")
+			
+func on_give_crop_seeds() -> void:
+	ToolManager.enable_tool_button(DataTypes.Tools.TillGround)
+	ToolManager.enable_tool_button(DataTypes.Tools.WaterCrops)
+	ToolManager.enable_tool_button(DataTypes.Tools.PlantCorn)
+	ToolManager.enable_tool_button(DataTypes.Tools.PlantTomato)
